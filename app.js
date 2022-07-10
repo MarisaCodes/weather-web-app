75b42338789fd54b0b825aa6ecce65b1e9d76908
MarisaCodes <marisakirisame955@gmail.com>
2022-07-10 05:04:51 +0400

    fixed a bug where using trash icon after a duplication error stops button click from working. Added a ton of proper comments on the app.js file. Final commit for sure

--- app.js
+++ app.js
@@ -17,31 +17,34 @@ let resource =
 // weather icon
 let icon = "";
 let iconLink = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+// div with class wrapper
 const wrapper = document.querySelector(".wrapper");
+// the search icon button
 const btn = document.querySelector("button");
 
-//console.log(btn.previousElementSibling);
+// when submitting user input, I am using button click and it happens to work well
 
 btn.addEventListener("click", (event) => {
-  event.preventDefault();
+  event.preventDefault(); //prevent page reload
   city = btn.previousElementSibling.value;
   resource =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     city +
-    "&appid=d3ea8ae7f71ead52761191ffe7ffc7e8&units=metric";
+    "&appid=d3ea8ae7f71ead52761191ffe7ffc7e8&units=metric"; //adding user input
 
   fetch(resource)
     .then((response) => {
-      return response.json();
+      return response.json(); //this returns a promise object, can use .then() method on it
     })
     .then((data) => {
       let form = btn.parentElement;
       let arrForm = Array.from(form.children);
       arrForm.forEach((element) => {
         if (element.classList.contains("invalid-input")) {
-          element.remove();
+          element.remove(); // cleaning all error messages (NOT just the message: invalid input X) after user searches data
         }
       });
+      //looping through existing weather cards to "test" whether a city has already been searched for
       let arrWeatherCards = Array.from(
         btn.parentElement.nextElementSibling.children
       );
@@ -53,31 +56,40 @@ btn.addEventListener("click", (event) => {
             duplicateWarn.innerText = `You already have the weather for ${data.name}!`;
             btn.parentElement.appendChild(duplicateWarn);
           }, 50);
-          test = true;
+          test = true; // test is changed to true if city already exists in cards
           break;
         } else {
           test = false;
         }
       }
       if (data && !test) {
+        //getting weather icon
         icon = data.weather[0].icon;
         iconLink = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+        //this is the string for the contents of a weather card
+
         let cardStr = ` <i class="fa-solid fa-xmark fa-2x"></i>
                         <div class="location">
-          <span class="city"></span> <span class="country"></span>
-        </div>
-        <p class="temperature"></p>
-        <img/>
-        <p class="description"></p>`;
+                            <span class="city"></span> 
+                            <span class="country"></span>
+                        </div>
+                        <p class="temperature"></p>
+                        <img/>
+                        <p class="description"></p>`;
+
         weatherCard = document.createElement("div");
         weatherCard.className = "weather-card";
         weatherCard.innerHTML = cardStr;
         wrapper.appendChild(weatherCard);
         weatherCard.querySelector(".city").innerText = data.name;
         weatherCard.querySelector(".country").innerText = data.sys.country;
-        weatherCard.querySelector(
-          ".temperature"
-        ).innerHTML = `<span></span><span><sup>&deg;C</sup><sup>|</sup><sup class="convert">&deg;F</sup></span>`;
+        weatherCard.querySelector(".temperature").innerHTML = `<span></span>
+                       <span>
+                          <sup>&deg;C</sup>
+                          <sup>|</sup>
+                          <sup class="convert">&deg;F</sup>
+                        </span>`;
         weatherCard.querySelector(
           ".temperature"
         ).firstElementChild.innerText = `${Math.ceil(data.main.temp)}`;
@@ -85,17 +97,17 @@ btn.addEventListener("click", (event) => {
           data.weather[0].description.toUpperCase();
         weatherCard.querySelector("img").src = iconLink;
       }
-      if (btn.nextElementSibling) {
-        if (btn.nextElementSibling.innerText == "Could not fetch data ❌") {
-          btn.nextElementSibling.remove();
-        }
-      }
     })
     .catch((err) => {
+      // console.logging the error for my own tests, I am not sure if it is necessary
       console.log("could not fetch data", err);
+
+      //if the button has a sibling element next to it (which will always be an error message) remove it...
       if (btn.nextElementSibling) {
         btn.nextElementSibling.remove();
       }
+
+      // ...then add this specific error message but after 50ms. I wanted to created a text blink effect or a refresh effect
       setTimeout(() => {
         let warningText = document.createElement("p");
         warningText.classList.add("invalid-input");
@@ -104,14 +116,17 @@ btn.addEventListener("click", (event) => {
       }, 50);
     });
 
+  //finally reset the input/search box so that it has nothing in it
+
   btn.previousElementSibling.value = "";
 });
 
-// deleting/clearing the whole page
+// deleting all cards/clearing the whole page
 
 const trash = document.querySelector(".fa-trash-can");
 
 trash.addEventListener("click", () => {
+  test = false;
   let arrWeatherCards = Array.from(
     btn.parentElement.nextElementSibling.children
   );
@@ -123,12 +138,15 @@ trash.addEventListener("click", () => {
   }
 });
 
-//deleting a weaather card
+//deleting an individual weaather card
 
 wrapper.addEventListener("click", (e) => {
   if (e.target.classList.contains("fa-xmark")) {
     e.target.parentElement.remove();
   }
+
+  // conversion from celsius to fahrenheit and vice versa
+  //the ".convert" css class just grays out the unit that is not currently in use
   if (e.target.classList.contains("convert")) {
     if (e.target.previousElementSibling) {
       if (e.target.previousElementSibling.previousElementSibling) {
