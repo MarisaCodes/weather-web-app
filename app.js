3ca9b4521b50e1a10ac538801128261788cd8d46
MarisaCodes <marisakirisame955@gmail.com>
2022-07-14 21:35:36 +0400

    refactored the code into reusable functions

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-// Weather API by city name
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
-// API key
-// d3ea8ae7f71ead52761191ffe7ffc7e8
-
-// weather icons link (example link for icon:11d)
-//https://openweathermap.org/img/wn/11d@2x.png
 let test = false; //test variable for whether a country name is repeated
 
 // div with class wrapper
@@ -13,15 +5,116 @@ const wrapper = document.querySelector(".wrapper");
 // the search icon button
 const btn = document.querySelector("button");
 
+// // functions
+
+// this function removes all error messages when the user searches something
+const removeAllErrors = () => {
+  let form = btn.parentElement;
+  let arrForm = Array.from(form.children);
+  arrForm.forEach((element) => {
+    if (element.classList.contains("invalid-input")) {
+      element.remove(); // cleaning all error messages (NOT just the message: invalid input X) after user searches data
+    }
+  });
+};
+// this function is for getting the city name input and putting it into the url so data could be fetched
+const getResource = (city = null) => {
+  return (
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=d3ea8ae7f71ead52761191ffe7ffc7e8&units=metric"
+  );
+};
+
+// this function is for getting the icon src
+
+const getWeatherIcon = (icon = null) => {
+  return "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+};
+
+// this function is for the error message
+const errorMessage = () => {
+  setTimeout(() => {
+    let warningText = document.createElement("p");
+    warningText.classList.add("invalid-input");
+    warningText.innerText = "Could not fetch data ❌";
+    btn.parentElement.appendChild(warningText);
+  }, 50);
+};
+
+// this function is for checking if a card already exists in the search results
+
+const checkExistingCards = (cityName = null) => {
+  let arrWeatherCards = Array.from(
+    btn.parentElement.nextElementSibling.children
+  );
+  for (let i = 0; i < arrWeatherCards.length; i++) {
+    //looping through existing weather cards to "test" whether a city has already been searched for
+
+    if (arrWeatherCards[i].querySelector(".city").innerText == cityName) {
+      setTimeout(() => {
+        let duplicateWarn = document.createElement("p");
+        duplicateWarn.classList.add("invalid-input");
+        duplicateWarn.innerText = `You already have the weather for ${cityName}!`;
+        btn.parentElement.appendChild(duplicateWarn);
+      }, 50);
+      test = true; // test is changed to true if city already exists in cards
+      break;
+    } else {
+      test = false;
+    }
+  }
+};
+
+// function for generating the weather card
+
+const getWeatherCard = (
+  iconUrl,
+  cityName,
+  countryInitials,
+  temperature,
+  weatherDescription
+) => {
+  //getting weather icon
+  let iconLink = getWeatherIcon(iconUrl);
+
+  //this is the string for the contents of a weather card
+
+  let cardStr = ` <i class="fa-solid fa-xmark fa-2x"></i>
+                          <div class="location">
+                              <span class="city"></span> 
+                              <span class="country"></span>
+                          </div>
+                          <p class="temperature"></p>
+                          <img/>
+                          <p class="description"></p>`;
+
+  weatherCard = document.createElement("div");
+  weatherCard.className = "weather-card";
+  weatherCard.innerHTML = cardStr;
+  wrapper.appendChild(weatherCard);
+  weatherCard.querySelector(".city").innerText = cityName;
+  weatherCard.querySelector(".country").innerText = countryInitials;
+  weatherCard.querySelector(".temperature").innerHTML = `<span></span>
+                         <span>
+                            <sup>&deg;C</sup>
+                            <sup>|</sup>
+                            <sup class="convert">&deg;F</sup>
+                          </span>`;
+  weatherCard.querySelector(
+    ".temperature"
+  ).firstElementChild.innerText = `${Math.round(temperature)}`;
+  weatherCard.querySelector(".description").innerText =
+    weatherDescription.toUpperCase();
+  weatherCard.querySelector("img").src = iconLink;
+};
+
 // when submitting user input, I am using button click and it happens to work well
 
 btn.addEventListener("click", (event) => {
   event.preventDefault(); //prevent page reload
-  let city = btn.previousElementSibling.value;
-  let resource =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&appid=d3ea8ae7f71ead52761191ffe7ffc7e8&units=metric"; //adding user input
+  removeAllErrors();
+  let resource = getResource(btn.previousElementSibling.value); //getting url from using getResource on user's input
 
   fetch(resource)
     .then((response) => {
@@ -32,65 +125,17 @@ btn.addEventListener("click", (event) => {
       return response.json(); //this returns a promise object, can use .then() method on it
     })
     .then((data) => {
-      let form = btn.parentElement;
-      let arrForm = Array.from(form.children);
-      arrForm.forEach((element) => {
-        if (element.classList.contains("invalid-input")) {
-          element.remove(); // cleaning all error messages (NOT just the message: invalid input X) after user searches data
-        }
-      });
-      //looping through existing weather cards to "test" whether a city has already been searched for
-      let arrWeatherCards = Array.from(
-        btn.parentElement.nextElementSibling.children
-      );
-      for (let i = 0; i < arrWeatherCards.length; i++) {
-        if (arrWeatherCards[i].querySelector(".city").innerText == data.name) {
-          setTimeout(() => {
-            let duplicateWarn = document.createElement("p");
-            duplicateWarn.classList.add("invalid-input");
-            duplicateWarn.innerText = `You already have the weather for ${data.name}!`;
-            btn.parentElement.appendChild(duplicateWarn);
-          }, 50);
-          test = true; // test is changed to true if city already exists in cards
-          break;
-        } else {
-          test = false;
-        }
-      }
+      // checking if a weather card already exists
+      checkExistingCards(data.name);
+
       if (data && !test) {
-        //getting weather icon
-        let icon = data.weather[0].icon;
-        let iconLink = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-
-        //this is the string for the contents of a weather card
-
-        let cardStr = ` <i class="fa-solid fa-xmark fa-2x"></i>
-                        <div class="location">
-                            <span class="city"></span> 
-                            <span class="country"></span>
-                        </div>
-                        <p class="temperature"></p>
-                        <img/>
-                        <p class="description"></p>`;
-
-        weatherCard = document.createElement("div");
-        weatherCard.className = "weather-card";
-        weatherCard.innerHTML = cardStr;
-        wrapper.appendChild(weatherCard);
-        weatherCard.querySelector(".city").innerText = data.name;
-        weatherCard.querySelector(".country").innerText = data.sys.country;
-        weatherCard.querySelector(".temperature").innerHTML = `<span></span>
-                       <span>
-                          <sup>&deg;C</sup>
-                          <sup>|</sup>
-                          <sup class="convert">&deg;F</sup>
-                        </span>`;
-        weatherCard.querySelector(
-          ".temperature"
-        ).firstElementChild.innerText = `${Math.ceil(data.main.temp)}`;
-        weatherCard.querySelector(".description").innerText =
-          data.weather[0].description.toUpperCase();
-        weatherCard.querySelector("img").src = iconLink;
+        getWeatherCard(
+          data.weather[0].icon,
+          data.name,
+          data.sys.country,
+          data.main.temp,
+          data.weather[0].description
+        );
       }
     })
     .catch((err) => {
@@ -102,18 +147,13 @@ btn.addEventListener("click", (event) => {
         btn.nextElementSibling.remove();
       }
 
-      // ...then add this specific error message but after 50ms. I wanted to created a text blink effect or a refresh effect
-      setTimeout(() => {
-        let warningText = document.createElement("p");
-        warningText.classList.add("invalid-input");
-        warningText.innerText = "Could not fetch data ❌";
-        btn.parentElement.appendChild(warningText);
-      }, 50);
+      // ...then add this specific error message
+      errorMessage();
     });
 
   //finally reset the input/search box so that it has nothing in it
 
-  btn.previousElementSibling.value = "";
+  btn.previousElementSibling.value = null;
 });
 
 // deleting all cards/clearing the whole page
@@ -151,16 +191,16 @@ wrapper.addEventListener("click", (e) => {
         );
         let tempStr =
           e.target.parentElement.parentElement.firstElementChild.innerText;
-        tempStr = String(Math.round((Number(tempStr) * 9) / 5 + 32));
+        tempStr = String(Math.round((Number(tempStr) * 9) / 5 + 32)); // conversion to degrees C
         e.target.parentElement.parentElement.firstElementChild.innerText =
           tempStr;
       }
-    } else if (e.target.nextElementSibling.nextElementSibling) {
+    } else {
       e.target.classList.remove("convert");
       e.target.nextElementSibling.nextElementSibling.classList.add("convert");
       let tempStr =
         e.target.parentElement.parentElement.firstElementChild.innerText;
-      tempStr = String(Math.round(((Number(tempStr) - 32) * 5) / 9));
+      tempStr = String(Math.round(((Number(tempStr) - 32) * 5) / 9)); // conversion to degrees F
       e.target.parentElement.parentElement.firstElementChild.innerText =
         tempStr;
     }
